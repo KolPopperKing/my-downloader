edc0773b8b75b3c51b6a2c5d9b51d84ea1c09e9b
KolPopperKing <popera898@gmail.com>
2026-05-25 18:08:27 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import yt_dlp
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+@app.get("/download")
+def download(url: str):
+    with yt_dlp.YoutubeDL({'format': 'best'}) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return {"download_url": info['url']}
